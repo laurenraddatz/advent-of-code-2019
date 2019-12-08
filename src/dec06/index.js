fa3de12f9b81656218a7494c29d2c5c378b372f3
lauren-raddatz <lauren.raddatz@workday.com>
2019-12-08 16:53:16 -0700

    day 6 part 2

--- src/dec06/index.js
+++ src/dec06/index.js
@@ -21,11 +21,49 @@ const getOrbits = () => {
       return 0
     }
   }
-  const getTotalOrbits = Object.keys(orbitMap)
+  const totalOrbits = Object.keys(orbitMap)
     .map(getOrbitCount)
     .reduce((total, curr) => total + curr, 0)
 
-  return getTotalOrbits
+  return totalOrbits
 }
 
 console.log('part one: ', getOrbits())
+
+const getDistance = () => {
+  const orbits = input.split('\n')
+  const orbitMap = {}
+
+  orbits.map((orbit) => {
+    const [a, b] = orbit.split(')')
+    orbitMap[b] = a
+  })
+
+  const distances = {}
+  let objects = Object.keys(orbitMap)
+
+  objects.map((object, idx) => {
+    const visited = []
+    let next = orbitMap[object]
+    let distance = 0
+    
+    while (next) {
+      next = orbitMap[next]
+      distance++
+
+      visited.push([next, distance])
+    }
+    distances[object] = visited
+  })
+
+  const youClosestParent = distances['YOU']
+    .filter(([object]) => distances['SAN'].find(([object2]) => object === object2))
+    .sort((a, b) => a[1] - b[1])[0]
+
+  const sanClosestParent = distances['SAN']
+    .find(([object]) => object === youClosestParent[0])
+
+  return youClosestParent[1] + sanClosestParent[1]
+}
+
+console.log('part two: ', getDistance())
